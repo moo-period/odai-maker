395014211bdcfc1fc7ed9228578d076c76616678
moo_period <be.makiya@gmail.com>
2019-01-31 22:40:56 +0900

    初期インポート

---
+++ src/main/java/tokyo/caprese/odai/OdaiMakerApplication.java
@@ -0,0 +1,15 @@
+package tokyo.caprese.odai;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class OdaiMakerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OdaiMakerApplication.class, args);
+    }
+}
